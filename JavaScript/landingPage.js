63327a4d249f022c6295fa28aec99a2256cadc36
Your Name <mechbuddy.shivam@gmail.com>
2021-10-02 18:09:21 +0530

    carousel

--- JavaScript/landingPage.js
+++ JavaScript/landingPage.js
@@ -52,3 +52,44 @@ function topFunction() {
 function goToHome() {
   window.location.assign("./index.html");
 }
+
+// Our brands Corousel
+
+let preBtn = document.querySelector('#preBtn');
+let nextBtn = document.querySelector('#nextBtn');
+
+	let product = document.getElementsByClassName('product')
+	let product_page = Math.ceil(product.length/4);
+	let l = 0;
+	let movePer = 25.34;
+	let maxMove = 203;
+	// mobile_view	
+	let mob_view = window.matchMedia("(max-width: 768px)");
+	if (mob_view.matches)
+	 {
+	 	movePer = 50.36;
+	 	maxMove = 504;
+	 }
+
+	let right_mover = ()=>{
+		l = l + movePer;
+		if (product == 1){l = 0; }
+		for(const i of product)
+		{
+			if (l > maxMove){l = l - movePer;}
+			i.style.left = '-' + l + '%';
+		}
+
+	}
+	let left_mover = ()=>{
+		l = l - movePer;
+		if (l<=0){l = 0;}
+		for(const i of product){
+			if (product_page>1){
+				i.style.left = '-' + l + '%';
+			}
+		}
+	}
+
+	preBtn.onclick = ()=>{left_mover();}
+	nextBtn.onclick = ()=>{right_mover();}
